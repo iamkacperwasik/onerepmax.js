8bbe2b490b7c337ee4bfc9419b1512a63d9773ff
Kacper Wąsik <kacper@wasik.me>
2023-09-09 23:11:29 +0200

    refactor: update method list

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,13 @@
-export {calculate_brzycki_1rm} from "./methods/Brzycki"
 export {calculate_epley_1rm} from "./methods/Epley"
-export {calculate_lombardi_1rm} from "./methods/Lombardi"
+export {calculate_brzycki_1rm} from "./methods/Brzycki"
+// Adams
+// Baechle
+// Berger
+// Brown
+// Kemmler
 export {calculate_lander_1rm} from "./methods/Lander"
-export {calculate_wathan_1rm} from "./methods/Wathan"
+export {calculate_lombardi_1rm} from "./methods/Lombardi"
 export {calculate_mayhew_szekely_1rm} from "./methods/MayhewSzekely"
-
-// Ermal Feigenbaum Method - calculateFeigenbaum1RM
-export const calculateFeigenbaum1RM = () => {}
-
-// Prilepin's Chart - calculatePrilepin1RM
-export const calculatePrilepin1RM = () => {}
-
-// Lombardi II Method - calculateLombardiII1RM
-export const calculateLombardiII1RM = () => {}
-
-// Lyle McDonald's Method - calculateLyleMcDonald1RM
-export const calculateLyleMcDonald1RM = () => {}
-
-// Zatsiorsky Method - calculateZatsiorsky1RM
-export const calculateZatsiorsky1RM = () => {}
-
-// Schwarzenegger Method - calculateSchwarzenegger1RM
-export const calculateSchwarzenegger1RM = () => {}
-
-// Evan Land Method - calculateEvanLand1RM
-export const calculateEvanLand1RM = () => {}
-
-// Zechs Method - calculateZechs1RM
-export const calculateZechs1RM = () => {}
-
-// Alan Aragon Method - calculateAlanAragon1RM
-export const calculateAlanAragon1RM = () => {}
+// Naclerio
+// OConner
+export {calculate_wathan_1rm} from "./methods/Wathan"
