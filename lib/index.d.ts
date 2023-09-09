cea43c36d49c1943ae7a2444cac055f0b3a36a39
Kacper Wąsik <kacper@wasik.me>
2023-09-09 19:34:48 +0200

    feat: generate types on build

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+export declare const calculateBrzycki1RM: () => void;
+export declare const calculateEpley1RM: () => void;
+export declare const calculateLombardi1RM: () => void;
+export declare const calculateOConor1RM: () => void;
+export declare const calculateLander1RM: () => void;
+export declare const calculateWathan1RM: () => void;
+export declare const calculateMayhewSzekely1RM: () => void;
+export declare const calculateFeigenbaum1RM: () => void;
+export declare const calculatePrilepin1RM: () => void;
+export declare const calculateLombardiII1RM: () => void;
+export declare const calculateLyleMcDonald1RM: () => void;
+export declare const calculateZatsiorsky1RM: () => void;
+export declare const calculateSchwarzenegger1RM: () => void;
+export declare const calculateEvanLand1RM: () => void;
+export declare const calculateZechs1RM: () => void;
+export declare const calculateAlanAragon1RM: () => void;
