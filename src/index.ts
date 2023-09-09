9497d72cb929af3924874a46fb6deb29dca5e451
Kacper Wąsik <kacper@wasik.me>
2023-09-10 01:26:24 +0200

    refactor: change export functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-export {calculate_epley_1rm} from "./methods/Epley"
-export {calculate_brzycki_1rm} from "./methods/Brzycki"
-export {calculate_adams_1rm} from "./methods/Adams"
-export {calculate_baechle_1rm} from "./methods/Baechle"
-export {calculate_berger_1rm} from "./methods/Berger"
-export {calculate_brown_1rm} from "./methods/Brown"
-export {calculate_kemmler_1rm} from "./methods/Kemmler"
-export {calculate_lander_1rm} from "./methods/Lander"
-export {calculate_lombardi_1rm} from "./methods/Lombardi"
-export {calculate_mayhew_szekely_1rm} from "./methods/MayhewSzekely"
-export {calculate_naclerio_1rm} from "./methods/Naclerio"
-export {calculate_oconner_1rm} from "./methods/OConner"
-export {calculate_wathan_1rm} from "./methods/Wathan"
+import {calculate_adams_1rm} from "./methods/Adams"
+import {calculate_baechle_1rm} from "./methods/Baechle"
+import {calculate_berger_1rm} from "./methods/Berger"
+import {calculate_brown_1rm} from "./methods/Brown"
+import {calculate_brzycki_1rm} from "./methods/Brzycki"
+import {calculate_epley_1rm} from "./methods/Epley"
+import {calculate_kemmler_1rm} from "./methods/Kemmler"
+import {calculate_lander_1rm} from "./methods/Lander"
+import {calculate_lombardi_1rm} from "./methods/Lombardi"
+import {calculate_mayhew_szekely_1rm} from "./methods/MayhewSzekely"
+import {calculate_naclerio_1rm} from "./methods/Naclerio"
+import {calculate_oconner_1rm} from "./methods/OConner"
+import {calculate_wathan_1rm} from "./methods/Wathan"
+
+export const calculate1RM = {
+  epley: calculate_epley_1rm,
+  brzycki: calculate_brzycki_1rm,
+  adams: calculate_adams_1rm,
+  baechle: calculate_baechle_1rm,
+  berger: calculate_berger_1rm,
+  brown: calculate_brown_1rm,
+  kemmler: calculate_kemmler_1rm,
+  lander: calculate_lander_1rm,
+  lombardi: calculate_lombardi_1rm,
+  mayhew_szekely: calculate_mayhew_szekely_1rm,
+  naclerio: calculate_naclerio_1rm,
+  oconner: calculate_oconner_1rm,
+  wathan: calculate_wathan_1rm,
+}
