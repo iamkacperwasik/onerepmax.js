0f0170a8eaeee055b36aa21e89b56d6da9dbb23d
Kacper Wąsik <kacper@wasik.me>
2023-09-09 05:26:06 +0200

    feat: add mockup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("hello world")
+// Brzycki Method - calculateBrzycki1RM
+export const calculateBrzycki1RM = () => {}
+
+// Epley Method - calculateEpley1RM
+export const calculateEpley1RM = () => {}
+
+// Lombardi Method - calculateLombardi1RM
+export const calculateLombardi1RM = () => {}
+
+// O'Conor Method - calculateOConor1RM
+export const calculateOConor1RM = () => {}
+
+// Lander Method - calculateLander1RM
+export const calculateLander1RM = () => {}
+
+// Wathan Method - calculateWathan1RM
+export const calculateWathan1RM = () => {}
+
+// Mayhew and Szekely Method - calculateMayhewSzekely1RM
+export const calculateMayhewSzekely1RM = () => {}
+
+// Ermal Feigenbaum Method - calculateFeigenbaum1RM
+export const calculateFeigenbaum1RM = () => {}
+
+// Prilepin's Chart - calculatePrilepin1RM
+export const calculatePrilepin1RM = () => {}
+
+// Lombardi II Method - calculateLombardiII1RM
+export const calculateLombardiII1RM = () => {}
+
+// Lyle McDonald's Method - calculateLyleMcDonald1RM
+export const calculateLyleMcDonald1RM = () => {}
+
+// Zatsiorsky Method - calculateZatsiorsky1RM
+export const calculateZatsiorsky1RM = () => {}
+
+// Schwarzenegger Method - calculateSchwarzenegger1RM
+export const calculateSchwarzenegger1RM = () => {}
+
+// Evan Land Method - calculateEvanLand1RM
+export const calculateEvanLand1RM = () => {}
+
+// Zechs Method - calculateZechs1RM
+export const calculateZechs1RM = () => {}
+
+// Alan Aragon Method - calculateAlanAragon1RM
+export const calculateAlanAragon1RM = () => {}
