06d61b6279444db5df4a801d0ca0278970e1c128
Kacper Wąsik <kacper@wasik.me>
2023-09-09 21:15:33 +0200

    refactor: remove lib folder

--- lib/methods/Brzycki.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Calculate 1 Repetition Maximum (1RM) using Brzycki Method.
- * @param weight_used - Weight used in the training (in kilograms).
- * @param repetitions - Number of repetitions performed.
- * @returns The estimated 1RM.
- */
-export declare const calculate_brzycki_1rm: (weight_used: number, repetitions: number) => number;
